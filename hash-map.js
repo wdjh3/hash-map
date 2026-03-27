96525aef83a3fb1ae0fd076ee897de3ade99036a
wdjh3 <198714742+wdjh3@users.noreply.github.com>
2026-03-27 16:38:27 +0800

    refactor: Make hash function modulus inside loop

--- hash-map.js
+++ hash-map.js
@@ -10,7 +10,7 @@ class HashMap {
 
     const primeNumber = 31;
     for (let i = 0; i < key.length; i++) {
-      hashCode = primeNumber * hashCode + key.charCodeAt(i);
+      hashCode = (primeNumber * hashCode + key.charCodeAt(i)) % this.#capacity;
     }
 
     return hashCode;
