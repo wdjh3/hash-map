0fdcd7df34ad195bf5abcc20c83c559d2e215861
wdjh3 <198714742+wdjh3@users.noreply.github.com>
2026-03-27 16:25:11 +0800

    feat: Add hash function to HashMap

--- hash-map.js
+++ hash-map.js
@@ -1,9 +1,20 @@
 class HashMap {
-    #capacity = 16;
-    #loadFactor = 0.75;
-    constructor() {
-        this.map = [];
+  #capacity = 16;
+  #loadFactor = 0.75;
+  constructor() {
+    this.map = [];
+  }
+
+  hash(key) {
+    let hashCode = 0;
+
+    const primeNumber = 31;
+    for (let i = 0; i < key.length; i++) {
+      hashCode = primeNumber * hashCode + key.charCodeAt(i);
     }
+
+    return hashCode;
+  }
 }
 
 export { HashMap };
